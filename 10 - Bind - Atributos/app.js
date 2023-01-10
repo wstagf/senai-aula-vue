9efd554e639ab10bc0372d2d7c890e4a602d3bb4
Thiago Augusto <wstagf@gmail.com>
2023-01-10 10:18:17 -0300

    Exercício 10 - Bind de Atributos

---
+++ 10 - Bind - Atributos/app.js
@@ -0,0 +1,7 @@
+new Vue({
+    el: '#app',
+    data: {
+        imageSrc: "https://storage.googleapis.com/vue-mastery.appspot.com/flamelink/media/vuemastery-graphical-link-96x56.png",
+        image2Src: "https://senaigoias.com.br/site/img/logo2019_senai_nova_branco.png"
+    }
+})
