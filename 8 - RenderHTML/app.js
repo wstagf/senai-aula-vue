86efd0e7202e19cd0ae515fae6175f5949fb4390
Thiago Augusto <wstagf@gmail.com>
2023-01-10 10:17:02 -0300

    Exercício 8 - Diretiva V-HTML

---
+++ 8 - RenderHTML/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        pagina: ` 
+        <div style="background-color:black;">
+        <h1 style="color:white;padding:30px;">Vue na veia</h1>
+        <p style="color:white;">Na veia, não na véia</p>
+        </div> `
+    }
+})
