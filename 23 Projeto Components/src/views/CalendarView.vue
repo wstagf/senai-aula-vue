f49e67bc324733653024af0eae017dc2bd8f3a72
Thiago Augusto <wstagf@gmail.com>
2023-01-12 12:04:49 -0300

    Finalizado exercicio 23

---
+++ 23 Projeto Components/src/views/CalendarView.vue
@@ -0,0 +1,28 @@
+
+<script>
+import VueInlineCalendar from 'vue2-inline-calendar'
+
+export default {
+  name: 'App',
+  components: {
+    VueInlineCalendar
+  },
+  data() {
+    return {
+      selectedDate: null
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="about">
+    <vue-inline-calendar @select-date="selectedDate = $event" />
+  </div>
+</template>
+
+<style>
+.about {
+  width: 50vw;
+}
+</style>
